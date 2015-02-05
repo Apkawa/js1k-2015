c6a0fd5977958fc8e3ffa13bf8c1bef83b35a0ad
apkawa <apkawa@gmail.com>
2015-02-05 23:28:50 +0300

    process

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,13 @@ var uglify = require('gulp-uglify');
 var jscrush = require('gulp-jscrush');
 var rename = require('gulp-rename');
 var closureCompiler = require('gulp-closure-compiler');
+var gcallback = require('gulp-callback');
+var gutil = require('gulp-util');
+var fs = require('fs');
 
 
-gulp.task('default', function () {
-    // place code for your default task here
-    gulp.src("./js1k_uncompress.js")
+gulp.task('closure-compile', function () {
+    gulp.src("./js1k.js")
         .pipe(closureCompiler({
                 compilerPath: 'bower_components/closure-compiler/lib/vendor/compiler.jar',
                 compilerFlags: {
@@ -17,10 +19,25 @@ gulp.task('default', function () {
             }
         ))
         .pipe(gulp.dest('./dest/'))
+});
 
+gulp.task('minify', ['closure-compile'], function () {
     gulp.src('./dest/js1k.compiled.js')
         .pipe(uglify())
         .pipe(jscrush())
         .pipe(rename('js1k.min.js'))
         .pipe(gulp.dest('./dest/'))
+        .pipe(gcallback(function () {
+            gutil.log('Source size', gutil.colors.magenta(
+                fs.statSync('./js1k.js')['size'] + ' bytes'))
+            gutil.log('Result size', gutil.colors.magenta(
+                fs.statSync('./dest/js1k.min.js')['size'] + ' bytes'))
+
+        }))
+});
+gulp.task('default', ['minify'], function () {
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./js1k.js', ['default'])
 });
--- js1k.js
+++ js1k.js
@@ -1,103 +1,134 @@
+rad = 250; // Выбранный радиал
+crs = 250; // текущий курс
+crad = 250; // Текущий радиал
 
-rad = 25.0; // Выбранный радиал
-crad = 21.5; // Текущий радиал
 tv = -1; // Направление на vor (-1 - на, +1 - от)
+nv = 0; // Текущий vor
 
-tau = 2 * Math.PI;
+vrs = [
+    {r: 250,
+        cr: 250,
+        tv: -1
+    },
+    {r: 96, cr: 50, tv: 1}
+]
+
 cx = a.width / 2;
-cy = a.height / 2;
+cy = a.height / 2 + 200;
+
+
+b.onkeydown = function (e) {
+    k = e.keyCode;
+    v = vrs[nv];
+    k == 37 && [v.cr--, D(0, 1)];
+    k == 39 && [v.cr++, D(0, 1)];
+    k == 38 && v.r--;
+    k == 40 && v.r++;
+    if (k == 32) nv ^= 1;
+    D(0);
+    D(1);
+    console.log(k)
+};
+
 pi = Math.PI;
+tau = 2 * pi;
 cos = Math.cos;
 sin = Math.sin;
 abs = Math.abs;
-i = -100;
 br = 150;
-sw = {0: "N", 90: "E", 180: "S", 270: "W"};
-ofs = (crad - rad)
+sw = "NESW";
+w = '#FFF'
 
-c.lineWidth = 2;
-c.strokeStyle = '#FFF';
 c.font = "25px arial";
 c.textAlign = "center";
+c.lineWidth = 2;
 
-bP = 'beginPath';
-f = 'fillStyle';
-t = 'translate';
-
-
-
+function D(n, t) {
+    y = cy - n*420;
+    x = cx - (t||0) * 400;
+    i = -100;
+    r = vrs[n].r
+    tv = vrs[n].tv
+    ofs = (vrs[n].cr - r);
+    c.strokeStyle = w;
+    c.translate(x, y);
 // Фон
-c[bP]();
-c.arc(cx, cy, 200, 0, 2 * pi, false);
-c[f] = 'black';
-c.fill();
-c.stroke();
+    c.arc(0, 0, 200, 0, 2 * pi, false);
+    c.fillStyle = '#000';
+    c.fill();
+
+    //// Фон текущего курса
+    //c.rect(-50, -280, 100, 50);
+    //c.fill()
+    //// Текущий курс
+    //c.fillText(crs, 0, -245);
 
 // Шкала отклонения от радиала
-for (; i < 120; i += 20) {
-    c[bP]();
-    c.arc(cx + i, cy, i ? 2 : 5, 0, 2 * Math.PI, false);
-    c.stroke();
-}
+    for (; i < 120; i += 20) {
+        c.beginPath();
+        c.arc(i, 0, i ? 2 : 8, 0, tau, false);
+        c.stroke();
+    }
 
 // Индикатор отклонения
-c[bP]();
-ofs = abs(ofs) > 5 ? 5 : ofs * 100 / 5;
-c.rect(cx + ofs - 2, cy - 100, 4, 200);
-c[f] = "#FFF";
-c.fill();
-c.stroke();
+    _o = abs(ofs) > 5 ? (ofs<0?-1:1) * 100 : ofs * 100 / 5;
+    c.rect(_o - 2, -100, 4, 200);
+    c.fillStyle = w;
+    c.fill();
+    c.stroke();
 
 
 // Направление на VOR
-c[bP]();
-c.moveTo(cx + 55, cy + 10 * tv);
-c.lineTo(cx + 55 + 30, cy + 10 * tv);
-c.lineTo(cx + 55 + 15, cy + 30 * tv);
-c.closePath();
-c.fill();
-c.stroke();
+    c.moveTo(55, 10 * tv);
+    c.lineTo(55 + 30, 10 * tv);
+    c.lineTo(55 + 15, 30 * tv);
+    c.fill();
+    c.stroke();
 
 // Указатель радиала
-c[bP]();
-c.moveTo(cx, cy - br + 5);
-c.lineTo(cx - 5, cy - br + 10);
-
-c.lineTo(cx + 5, cy - br + 10);
-c.closePath();
-c.fill();
-c.stroke();
-
-// Настройка на радиал
-c[t](cx, cy);
-c.rotate(rad * pi / 180);
-c[t](-cx, -cy);
-
-for (i = 0; i < 360; i += 5) {
-
-    var aR = (tau * i / 360) - (tau / 4),
-        ls = i % 10 ? 5 : i % 30 ? 10 : 15,
-        car = cos(aR),
-        sar = sin(aR),
-        fx = cx + car * br,
-        fy = cy + sar * br,
-        tx = fx + car * ls,
-        ty = fy + sar * ls,
-        nx = tx + car * 5,
-        ny = ty + sar * 5
-        ;
-    c[bP]();
-    c.moveTo(fx, fy);
-    c.lineTo(tx, ty);
+    c.moveTo(0, -145);
+    c.lineTo(-5, -140);
+    c.lineTo(5, -140);
+    c.fill();
     c.stroke();
+
+// Настройка на радиал (поворот шкалы)
     c.save();
-    if ((i % 30) == 0) {
-        c[t](nx, ny);
-        c.rotate(i * pi / 180);
-        c[t](-nx, -ny);
-        c.fillText(sw[i] || i.toString(), nx, ny);
-        c.restore();
+    c.rotate(-r * pi / 180);
+    c.translate(-x, -y);
+
+    for (i = 0; i < 360; i += 5) {
+        aR = (tau * i / 360) - (tau / 4);
+        ls = i % 10 ? 5 : i % 30 ? 10 : 15;
+        car = cos(aR);
+        sar = sin(aR);
+        fx = x + car * br;
+        fy = y + sar * br;
+        tx = fx + car * ls;
+        ty = fy + sar * ls;
+        nx = tx + car * 5;
+        ny = ty + sar * 5;
+
+        c.moveTo(fx, fy);
+        c.lineTo(tx, ty);
+        c.stroke();
+
+        if ((i % 30) == 0) {
+            c.save();
+            c.translate(nx, ny);
+            c.rotate(i * pi / 180);
+            c.translate(-nx, -ny);
+            c.fillText(i % 90 ? i : sw[i / 90], nx, ny);
+            c.restore();
+        }
     }
+    c.restore()
+    c.translate(-x, -y);
+    c.closePath();
+
 }
 
+D(1);
+D(0);
+D(0, 1);
 
